8cad0438a396969fce1afa5cfecc93a03c86b10c
backslashV <8752221+backslashV@users.noreply.github.com>
2022-05-19 00:15:56 -0700

    Fix index out of range for inputTags

--- bmw.py
+++ bmw.py
@@ -18,48 +18,68 @@ USERNAME = "example@email.com"
 PASSWORD = "your_my_garage_password"
 
 MY_GARAGE_URL = "https://mygarage.bmwusa.com/"
+NUM_INPUT_TAGS = 2
+CREDENTIALS_TAG = "input"
+LOGIN_BUTTON_CLASS_NAME = ".custom-button.__b.primary"
 VEHICLE_VIN_CLASS_NAME = "mbg__vehicle-vinprod"
+VEHICLE_PRODUCTION_STATUS_TAG = "div"
 VEHICLE_PRODUCTION_STATUS_CLASS_NAME = "mbg__vehicle-status"
 TIMEOUT = 10 # in seconds
 
-options = Options()
-options.add_argument('--no-sandbox')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--disable-extensions')
-options.add_argument('--headless')
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.binary_location = CHROME_BINARY_PATH
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+def main():
+    global driver
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--headless')
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.binary_location = CHROME_BINARY_PATH
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-try:
-    driver.get(MY_GARAGE_URL)
-except Exception as ex:
-    exit(ex)
+    try:
+        driver.get(MY_GARAGE_URL)
+    except Exception as ex:
+        exit(ex)
 
-# find the username and password fields
-inputTags = driver.find_elements(By.TAG_NAME, "input")
-usernameField = inputTags[0]
-passwordField = inputTags[1]
-loginButton = driver.find_elements(By.CSS_SELECTOR, ".custom-button.__b.primary")[0]
+    # wait for the login screen to fully load
+    implicitWaitForPageLoad(By.CSS_SELECTOR, LOGIN_BUTTON_CLASS_NAME)
+    
+    # log in
+    performLogin(CREDENTIALS_TAG, LOGIN_BUTTON_CLASS_NAME, USERNAME, PASSWORD)
 
-usernameField.send_keys(USERNAME)
-passwordField.send_keys(PASSWORD)
-loginButton.click()
+    # wait for my garage to fully load
+    implicitWaitForPageLoad(By.CLASS_NAME, VEHICLE_VIN_CLASS_NAME)
 
-# wait for the url to become my garage
-try:
-    WebDriverWait(driver, TIMEOUT).until(EC.url_matches(MY_GARAGE_URL))
-except Exception as ex:
-    exit("Timeout occured waiting for My Garage URL")
+    # fetch the production status
+    productionStatus = getProductionStatus(BeautifulSoup(driver.page_source, 'html.parser'),
+            VEHICLE_PRODUCTION_STATUS_TAG, VEHICLE_PRODUCTION_STATUS_CLASS_NAME)
 
-# wait for my garage to fully load
-try:
-    WebDriverWait(driver, TIMEOUT).until(EC.text_to_be_present_in_element((By.CLASS_NAME, VEHICLE_VIN_CLASS_NAME), "PRODUCTION #: "))
-except:
-    exit("My Garage took too long to load. Quitting...")
+    print(productionStatus.text.split(":  ", 1)[1])
 
-html = BeautifulSoup(driver.page_source, 'html.parser')
-productionStatus = html.find_all("div", {"class": VEHICLE_PRODUCTION_STATUS_CLASS_NAME})[0]
+def getProductionStatus(html, production_status_tag, production_status_class_name):
+    return html.find_all(production_status_tag, {"class": production_status_class_name})[0]
 
-print(productionStatus.text.split(":  ", 1)[1])
+def performLogin(input_tag, login_button_class, username, password):
+    # find the username and password fields
+    inputTags = driver.find_elements(By.TAG_NAME, input_tag)
+    if len(inputTags) < NUM_INPUT_TAGS:
+        exit("Not all input tags were found. Quitting...")
+    usernameField = inputTags[0]
+    passwordField = inputTags[1]
+    loginButton = driver.find_elements(By.CSS_SELECTOR, login_button_class)[0]
+
+    usernameField.send_keys(username)
+    passwordField.send_keys(password)
+    loginButton.click()
+
+def implicitWaitForPageLoad(locator, locatorValue):
+    try:
+        WebDriverWait(driver, TIMEOUT).until(EC.visibility_of_any_elements_located((locator,
+                locatorValue)))
+    except:
+        exit("implicitWaitForPageLoad timed out - locatorValue: " + locatorValue)
+
+if __name__ == "__main__":
+  main()
